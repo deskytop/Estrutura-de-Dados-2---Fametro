2052fe98b2d61fa2398cd761ae16166801b0f801
desky <daniel.melovieir@gmail.com>
2025-09-15 16:24:01 -0400

    resolução questão 2

---
+++ lista_exercicios_1_ponteiros/questao_2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+// Encontrar o maior e menor valor do array
+void encontrarMaiorMenor(int *array, int tamanho, int *maior, int *menor) {
+    *maior = array[0]; // inicializa com o primeiro elemento
+    *menor = array[0];
+    for (int i = 1; i < tamanho; i++) {
+        if (array[i] > *maior) {
+            *maior = array[i];
+        }
+        if (array[i] < *menor) {
+            *menor = array[i];
+        }
+    }
+}
+
+void main() {
+    int n;
+    printf("Digite o tamanho do array: ");
+    scanf("%d", &n);
+    int valores[n];
+    for (int i = 0; i < n; i++) {
+        printf("Digite o valor %d: ", i + 1);
+        scanf("%d", &valores[i]);
+    }
+    int maior, menor;
+    // Chama a função
+    encontrarMaiorMenor(valores, n, &maior, &menor);
+    printf("\nMaior valor: %d\n", maior);
+    printf("Menor valor: %d\n", menor);
+}
